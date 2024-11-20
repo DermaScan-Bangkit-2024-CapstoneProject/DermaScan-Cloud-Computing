2df5062d654bb2223cc93a8a361223f366166202
Andi Yahya <akunbelajarke1@gmail.com>
2024-11-21 01:01:16 +0700

    menambah fungsionalitas user login, user update dan melakukan update beberapa fungsi

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,14 +1,57 @@
 import db from "../database/connect-db.js";
+const userEmail = "user1@gmail.com";
+const signup = async (req, res) => {
+    try {
+        if (userEmail === "user2@gmail.com") {
+            res.status(400).send({
+                message: "User already exists",
+            });
+            return;
+        }
+        const userData = await db.collection("users").doc(userEmail).set({
+            name: "Udin Purwantio",
+            age: 22,
+            college: "UMB",
+            email: userEmail,
+            password: "123456",
+        });
 
-const register = (req, res) => {
-    res.send("Register");
+        res.status(201).send({
+            message: "User Successfully Created",
+        });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const dataLogin = {
+            email: userEmail,
+            password: "123456",
+        };
+        const usersCollection = db.collection("users");
+        const usersDoc = await usersCollection.where("email", "=", dataLogin.email).get();
+        if (usersDoc.empty) {
+            res.status(404).json({
+                message: "Data not found",
+            });
+            return;
+        }
+        res.status(200).json({
+            message: "Login Successfully",
+            token: "xyahs7236",
+        });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 const get = async (req, res) => {
     let userData = [];
     const usersCollection = db.collection("users");
     // const usersDoc = await usersCollection.get();
-    const usersDoc = await usersCollection.where("college", "=", "UNY").get(); //query data user by college
+    const usersDoc = await usersCollection.where("email", "=", userEmail).get(); //query data user by college
     if (usersDoc.empty) {
         res.status(404).json({
             message: "Data not found",
@@ -23,28 +66,33 @@ const get = async (req, res) => {
         });
     }
 };
-const login = (req, res) => {
-    res.send("login");
-};
-const signup = async (req, res) => {
-    try {
-        const userEmail = "tes6@gmail.com";
-        const userData = await db.collection("users").doc(userEmail).set({
-            name: "Putra",
-            age: 20,
-            college: "UMB",
-            email: userEmail,
-        });
 
-        res.status(200).send({
-            message: "User Success Created",
+const update = async (req, res) => {
+    let data = [];
+    try {
+        const usersCollection = db.collection("users");
+        const userData = await usersCollection.doc(userEmail).update({
+            age: 30,
+        });
+        const updatedData = await usersCollection.where("email", "=", userEmail).get();
+        updatedData.forEach((doc) => {
+            data.push(doc.data());
+        });
+        res.status(201).json({
+            message: "User Successfully Updated",
+            data: data.at(0),
         });
     } catch (error) {
-        res.status(500).send({ message: error.message });
+        res.status(500).send({
+            message: {
+                error: error.message,
+            },
+        });
     }
 };
+
 const logout = (req, res) => {
     res.send("logout");
 };
 
-export default { register, login, signup, logout, get };
+export default { update, login, signup, logout, get };
--- src/route/private-api.js
+++ src/route/private-api.js
@@ -3,7 +3,7 @@ import userController from "../controller/user-controller.js";
 const privateRouter = new express.Router();
 
 privateRouter.get("/api/users/current", userController.get);
-privateRouter.get("/api/update", userController.logout);
+privateRouter.get("/api/update", userController.update);
 privateRouter.get("/api/logout", userController.logout);
 
 export { privateRouter };

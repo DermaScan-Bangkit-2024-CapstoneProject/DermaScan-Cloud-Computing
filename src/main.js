6838cc217b8a80966762d1372e46bbd2b8b3b016
Muhammad Nizar <mnizarzr@gmail.com>
2024-11-30 02:16:31 +0700

    add .env.example and nodemon as dev depd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import web from "./app/web.js";
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.DEV_PORT; //port development = 3000; port production = 8080
+const PORT = process.env.PORT; //port development = 3000; port production = 8080
 
 web.listen(PORT, () => {
     console.info(`App start on http://localhost:${PORT}/api`); //jalankan dengan perintah "npm run start-dev"

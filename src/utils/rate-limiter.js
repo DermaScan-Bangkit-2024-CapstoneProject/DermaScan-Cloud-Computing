107eb34609aa5f00adb242b9248ddf11dfa79f63
Andi Yahya <akunbelajarke1@gmail.com>
2024-12-04 00:26:05 +0700

    menambahkan rate limiter: email limiter

---
+++ src/utils/rate-limiter.js
@@ -0,0 +1,11 @@
+import rateLimit from "express-rate-limit";
+
+const emailLimiter = rateLimit({
+    windowMs: 35 * 60 * 1000, // 35 minutes
+    max: 3, // limit each IP to 3 requests per windowMs
+    message: { message: "Too many requests, please try again after 35 minutes" },
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+export { emailLimiter };

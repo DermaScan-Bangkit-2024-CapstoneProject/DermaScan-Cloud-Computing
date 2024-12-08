edce397a2e716e81991f2d7a6ab431f37de6e7db
Andi Yahya <akunbelajarke1@gmail.com>
2024-12-08 15:25:23 +0700

    menambahkan fitur endpoint get diagnosis histories, get diagnosis history by id  dan delete diagnosis history

--- src/utils/image-storage-handler.js
+++ src/utils/image-storage-handler.js
@@ -1,34 +1,36 @@
 import { Storage } from "@google-cloud/storage";
 import { ResponseError } from "../error/error.js";
 import crypto from "crypto";
+import { response } from "express";
 const storage = new Storage({
     keyFilename: process.env.KEY_FILENAME,
     projectId: process.env.PROJECT_ID,
 });
+
 async function upload(imageFile, userId) {
-    console.log(imageFile);
     try {
         const bucketName = process.env.BUCKET_NAME;
         const bucket = storage.bucket(bucketName);
         const cryptoId = crypto.randomBytes(3).toString("hex");
         const imageId = `${imageFile.originalname.split(".")[0]}_${cryptoId}_${Date.now()}`;
-        const imageFileName = `users_histories/${userId}/${imageId}.${imageFile.originalname.split(".")[1]}`;
+        // const imageFileName = `users_histories/${userId}/${imageId}.${imageFile.originalname.split(".")[1]}`;
+        const imageFileName = `${imageId}.${imageFile.originalname.split(".")[1]}`;
         const blob = bucket.file(imageFileName);
         const blobStream = blob.createWriteStream({
             metadata: {
-                contentType: imageFile.mimetype, // Tetapkan MIME type
+                contentType: imageFile.mimetype,
             },
         });
         await new Promise((resolve, reject) => {
             blobStream.on("error", (err) => {
-                reject(new ResponseError(500, err.message)); // Menangani error stream
+                reject(new ResponseError(500, err.message));
             });
 
             blobStream.on("finish", () => {
                 resolve("File uploaded successfully");
             });
 
-            blobStream.end(imageFile.buffer); // Kirim buffer sebagai konten file
+            blobStream.end(imageFile.buffer);
         });
         return {
             image: `https://storage.googleapis.com/dermascan-storage/${imageFileName}`,
@@ -39,4 +41,13 @@ async function upload(imageFile, userId) {
     }
 }
 
-export { upload };
+async function deleteImage(imageUrl, diagId) {
+    const bucketName = process.env.BUCKET_NAME;
+    const bucket = storage.bucket(bucketName);
+    const parts = imageUrl.split("/");
+    imageUrl = parts[parts.length - 1];
+    console.log(imageUrl);
+    const deleteResult = await bucket.file(imageUrl).delete();
+    return deleteResult;
+}
+export { upload, deleteImage };
